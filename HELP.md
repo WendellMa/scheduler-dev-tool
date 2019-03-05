9592ca3d454a6820038a94c3471d2358125ed109
mayanqing <mayanqing@romens.cn>
2019-03-05 19:11:19 +0800

    feat(全局): 创建项目

---
+++ HELP.md
@@ -0,0 +1,7 @@
+# Getting Started
+
+### Guides
+The following guides illustrates how to use certain features concretely:
+
+* [Quick Start](https://github.com/mybatis/spring-boot-starter/wiki/Quick-Start)
+
---
+++ app/src/main/java/com/abigail/core/SchedulerDevToolApplication.java
@@ -0,0 +1,13 @@
+package com.abigail.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerDevToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerDevToolApplication.class, args);
+    }
+
+}
