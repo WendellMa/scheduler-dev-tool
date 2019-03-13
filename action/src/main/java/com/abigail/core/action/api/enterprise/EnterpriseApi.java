23ba0e6ab60cee718ca481c214af17ab7810044d
mayanqing <mayanqing@romens.cn>
2019-03-13 14:24:02 +0800

    feat(局部): 更新spring fox版本为2.9.2

--- action/src/main/java/com/abigail/core/action/api/enterprise/EnterpriseApi.java
+++ action/src/main/java/com/abigail/core/action/api/enterprise/EnterpriseApi.java
@@ -2,24 +2,34 @@ package com.abigail.core.action.api.enterprise;
 
 import com.abigail.core.domain.bo.enterprise.EnterpriseInfo;
 import com.abigail.core.domain.co.ErrorResponseResult;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Api(
         value = "enterprise",
-        description = "企业操作接口"
+        description = "企业操作接口",
+        tags = {"enterprise"}
 )
+@RequestMapping(value = "/action/enterprise",produces = "application/json")
 public interface EnterpriseApi {
     @ApiOperation(
+            nickname = "listEnterprise",
+            httpMethod = "GET",
             value = "获取企业列表",
             notes = "获取企业列表",
-            tags = {"enterprise"})
+            tags = {"enterprise"}
+//            ,
+//            extensions = {
+//                    @Extension(name = "x-ups-permissionName", properties = {
+//                            @ExtensionProperty(value = "", name = "")
+//                    })
+//
+//            }
+    )
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "OK"),
             @ApiResponse(code = 400, message = "Bad Request", response = ErrorResponseResult.class),
@@ -28,6 +38,6 @@ public interface EnterpriseApi {
             @ApiResponse(code = 404, message = "Not Found", response = ErrorResponseResult.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponseResult.class)
     })
-    @GetMapping(value = "/action/enterprise",produces = {"application/json"})
+    @GetMapping()
     ResponseEntity<List<EnterpriseInfo>> listEnterprise();
 }
--- action/src/main/java/com/abigail/core/action/controller/enterprise/EnterpriseController.java
+++ action/src/main/java/com/abigail/core/action/controller/enterprise/EnterpriseController.java
@@ -2,14 +2,17 @@ package com.abigail.core.action.controller.enterprise;
 
 import com.abigail.core.action.api.enterprise.EnterpriseApi;
 import com.abigail.core.domain.bo.enterprise.EnterpriseInfo;
+import com.abigail.core.domain.co.ErrorResponseResult;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class EnterpriseController implements EnterpriseApi {
 
     @Override
